5d3e33f421cbac276108afa5e7a084ce1220255f
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-07-25 20:30:11 -0700

    Added models for blog posts. Moved local settings out of global_settings.py

--- website/global_settings.py
+++ website/global_settings.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')$oovs3(k-iy%v&6wh14dslrt98^p3+1sbcn)$7^lvzs^lzd*c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,18 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -106,7 +81,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=500)
+    def __str__(self):
+        return self.name
+    
+class BlogPost(models.Model):
+    title = models.CharField(max_length=1000)
+    post_content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    date_posted = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.title
